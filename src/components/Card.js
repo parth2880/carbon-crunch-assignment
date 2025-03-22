333c00bde88d46cf207185cd3b7ac13b1772706c
Parth <parthsharma2880@gmail.com>
2025-03-22 17:29:38 +0530

    Added: micro interaction

--- src/components/Card.js
+++ src/components/Card.js
@@ -7,8 +7,8 @@ const Card = ({
   description, 
   className = '' 
 }) => {
-  return (
-    <div className={`bg-white rounded-lg p-6 ${className}`}>
+  return (  
+    <div className={`overflow-hidden relative group transition-all duration-300 bg-white rounded-lg p-6 hover:scale-101 hover:shadow-sm ${className}`}>
       <div className="size-[60px] bg-green-100 flex items-center justify-center rounded-3xl mb-6">
         {icon}
       </div>
@@ -20,6 +20,8 @@ const Card = ({
       <p>
         {description}
       </p>
+
+      <div className="absolute bottom-0 right-0 translate-x-[101%] group-hover:translate-x-0 transition-transform duration-300 px-2 py-1 rounded-tl-md cursor-pointer text-sm bg-primary text-white z-10">Read more</div>
     </div>
   );
 };
