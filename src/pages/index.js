b37f0967d0d3492d8cd2e68eef399f7adb66d376
Parth <parthsharma2880@gmail.com>
2025-03-22 17:14:30 +0530

    Updated: index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,113 +1,138 @@
 import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
+import Card from "@/components/Card";
+import StatCard from "@/components/StatCard";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const CubeIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    fill="currentColor"
+    className="size-10 text-primary"
+  >
+    <path d="M12.378 1.602a.75.75 0 0 0-.756 0L3 6.632l9 5.25 9-5.25-8.622-5.03ZM21.75 7.93l-9 5.25v9l8.628-5.032a.75.75 0 0 0 .372-.648V7.93Zm-10.5 14.25v-9l-9-5.25v8.57a.75.75 0 0 0 .372.648l8.628 5.033Z" />
+  </svg>
+);
 
 export default function Home() {
+  const cardsData = [
+    {
+      title: "Automated Data Collection",
+      description:
+        "Our system automates carbon data collection, saving time, reducing errors, and ensuring accurate sustainability reporting.",
+      icon: <CubeIcon />,
+    },
+    {
+      title: "Monitoring & Reporting",
+      description:
+        "Effortlessly track and report carbon emissions with automated monitoring, ensuring accuracy and compliance in sustainability reporting.",
+      icon: <CubeIcon />,
+    },
+    {
+      title: "Monitoring & Reporting",
+      description:
+        "Effortlessly track and report carbon emissions with automated monitoring, ensuring accuracy and compliance in sustainability reporting.",
+      icon: <CubeIcon />,
+    },
+    {
+      title: "Simplified Certification Process",
+      description:
+        "Streamline your certification process with our simplified, automated solution, reducing complexity and ensuring faster compliance.",
+      icon: <CubeIcon />,
+    },
+    {
+      title: "AI-Driven Insights",
+      description:
+        "Leverage AI-driven insights to uncover hidden patterns, optimize sustainability strategies, and drive impactful decision-making.",
+      icon: <CubeIcon />,
+    },
+    {
+      title: "AI-Driven Insights",
+      description:
+        "Leverage AI-driven insights to uncover hidden patterns, optimize sustainability strategies, and drive impactful decision-making.",
+      icon: <CubeIcon />,
+    },
+  ];
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/pages/index.js
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+    <div className="max-w-screen-2xl mx-auto py-10 px-4">
+      {/* Features heading section */}
+      <div className="uppercase text-primary mb-2 lg:text-xl font-bold py-1.5 lg:py-2.5 px-8 lg:px-14 bg-white tracking-wider max-w-max mx-auto rounded-full">
+        Features
+      </div>
+      <h1 className="text-4xl lg:text-6xl font-bold text-center">
+        Why Carbon Crunch?
+      </h1>
+
+      {/* Regular cards grid */}
+      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 mt-10 lg:mt-20">
+        {cardsData.map((card, index) => (
+          <Card
+            key={index}
+            icon={card.icon}
+            title={card.title}
+            description={card.description}
+          />
+        ))}
+      </div>
+
+      {/* Carbon Earth with ESG Statistics Cards */}
+      <div className="relative lg:mt-14">
+        <div className="relative flex flex-col lg:flex-row">
+          {/* Center image */}
+          <div className="lg:min-w-max">
             <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+              src="/images/carbon-earth.webp"
+              alt="Earth in glass dome with pollution"
+              width={838}
+              height={996}
+              objectFit="contain"
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+          </div>
+          <div className="relative lg:w-[35%] lg:mt-20 max-lg:space-y-4">
+            <div className="lg:absolute top-20 right-0 lg:translate-x-[0%] z-[4]">
+              <StatCard
+                largeTextClassName="text-2xl lg:text-[86px]"
+                titleClassName="text-xl lg:text-3xl"
+                largeText="3X"
+                title="ESG High Performers Deliver A Higher Total Shareholder Return"
+                className="w-full lg:max-w-[426px] bg-primary text-3xl"
+              />
+            </div>
+
+            <div className="lg:absolute top-1/3 left-0 lg:translate-x-[-52%] z-[3]">
+              <StatCard
+                largeTextClassName="text-2xl lg:text-[56px]"
+                titleClassName="text-xl lg:text-3xl"
+                largeText="98%"
+                title="Of CEOs Agree Sustainability Is Their Responsibility"
+                className="w-full lg:max-w-[492px] bg-[#3a3a3a] text-white"
+              />
+            </div>
+
+             <div className="lg:absolute bottom-60 right-0 lg:-translate-x-[10%] z-[2]">
+              <StatCard
+                largeTextClassName="text-2xl lg:text-[50px]"
+                titleClassName="text-xl lg:text-2xl lg:pl-4"
+                largeText="18%"
+                title="Of Companies Are Cutting Emissions Fast Enough To Reach Net Zero By 2050"
+                className="w-full lg:max-w-[380px] bg-foreground text-white lg:text-right"
+              />
+            </div>
+
+            <div className="lg:absolute bottom-30 left-0 lg:translate-x-[-64%] z-[1]">
+              <StatCard
+                largeTextClassName="text-2xl lg:text-[44px]"
+                titleClassName="text-xl lg:text-2xl lg:pr-20"
+                largeText="37%"
+                title="Of The World's Largest Companies Have A Public Net Zero Target. Nearly All Are Off Track"
+                className="w-full lg:max-w-[486px] bg-white text-primary"
+              />
+            </div>
+
+           
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
     </div>
   );
 }
